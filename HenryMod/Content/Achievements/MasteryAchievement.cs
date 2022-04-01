2453355801564a46ee318db6bdcf6fe73f2d5117
nayDPz <88465294+nayDPz@users.noreply.github.com>
2022-04-01 14:20:04 -0400

    Initial commit

---
+++ HenryMod/Content/Achievements/MasteryAchievement.cs
@@ -0,0 +1,17 @@
+﻿using RoR2;
+using System;
+using UnityEngine;
+
+namespace HenryMod.Modules.Achievements
+{
+    internal class MasteryAchievement : BaseMasteryUnlockable
+    {
+        public override string AchievementTokenPrefix => HenryPlugin.DEVELOPER_PREFIX + "_HENRY_BODY_MASTERY";
+        public override string AchievementSpriteName => "texMasteryAchievement";
+        public override string PrerequisiteUnlockableIdentifier => HenryPlugin.DEVELOPER_PREFIX + "_HENRY_BODY_UNLOCKABLE_REWARD_ID";
+
+        public override string RequiredCharacterBody => "HenryBody";
+                                                             //difficulty coeff 3 is monsoon. 3.5 is typhoon for grandmastery skins
+        public override float RequiredDifficultyCoefficient => 3;
+    }
+}
